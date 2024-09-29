ca40e684427efe1b4e5ea2318e13a1a775a887cd
TheHydra778 <fosterbryce777@yahoo.com>
2024-09-29 12:31:05 -0500

    reverted a lot of changes~

--- Bryce_Foster_Resume_Website/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-
-export const routes: Routes = [
-    {path: '', component: HomeComponent, data: {animation: 'HomePage'}},
-    {path: '**', component: NotfoundComponent}
-];
